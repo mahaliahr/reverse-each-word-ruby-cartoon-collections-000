3bc5001f35a0c6161ba4011959de0c6541e1bc67
mahaliahr <mahalia_hr@outlook.com>
2018-08-31 18:32:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,5 +6,14 @@ def reverse_each_word(sentence)
     new << words
   end 
   new.join(" ")
-  
+end 
+
+def reverse_each_word(sentence)
+  arr = sentence.split(" ")
+  new = []
+  arr.each do |words|
+    words.reverse!
+    new << words
+  end 
+  new.join(" ")
 end 
